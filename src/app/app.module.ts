717e2cd69badf327940cb9514be795a1d9832e9d
Noelia M <mm.noe2011@gmail.com>
2020-05-20 18:15:18 +0200

    Fin SpotiApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http';
-
+//Services
+import {SpotifyService} from './services/spotify.service';
+//Components
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-
-//Services
-import {SpotifyService} from './services/spotify.service';
+//Pipes
 import { NoimagePipe } from './pipes/noimage.pipe';
-import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
-import { LoadingComponent } from './components/shared/loading/loading.component';
+import { DomseguroPipe} from './pipes/domseguro.pipe';
 
 @NgModule({
   declarations: [
@@ -23,10 +24,12 @@ import { LoadingComponent } from './components/shared/loading/loading.component'
     HomeComponent,
     SearchComponent,
     ArtistaComponent,
-    NavbarComponent,
-    NoimagePipe,
+    NavbarComponent,    
     TarjetasComponent,
-    LoadingComponent
+    LoadingComponent,
+
+    NoimagePipe,
+    DomseguroPipe
   ],
   imports: [
     BrowserModule,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,14 +12,21 @@ import {SpotifyService} from '../../services/spotify.service';
 export class HomeComponent implements OnInit {  
   nuevosLanzamientos:any[]=[];
   loading:boolean;
+  error:boolean;
+  mensajeError:string;
 
   constructor(private spotyfyService: SpotifyService ) { 
     this.loading=true;
+    this.error=false
 
  this.spotyfyService.getNewReleases()
     .subscribe((resp:any)=>{
-      this.nuevosLanzamientos=resp;
+      this.nuevosLanzamientos=resp;  
       this.loading=false;
+      }, (err)=>{
+        this.loading=false;
+        this.error=true;
+        this.mensajeError=err.error.error.message;
       });
     }
 
